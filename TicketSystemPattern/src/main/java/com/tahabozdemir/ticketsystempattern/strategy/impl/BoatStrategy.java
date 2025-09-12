63a8550b66d49b55652b09840d43047fdc0d74c5
Taha Bozdemir <taha@bozdemir.net>
2025-09-12 15:00:31 +0300

    feat: implement route management in travel strategies

--- TicketSystemPattern/src/main/java/com/tahabozdemir/ticketsystempattern/strategy/impl/BoatStrategy.java
+++ TicketSystemPattern/src/main/java/com/tahabozdemir/ticketsystempattern/strategy/impl/BoatStrategy.java
@@ -1,12 +1,28 @@
 package com.tahabozdemir.ticketsystempattern.strategy.impl;
 
+import com.tahabozdemir.ticketsystempattern.model.Route;
 import com.tahabozdemir.ticketsystempattern.strategy.TravellingStrategy;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 @Component
 public class BoatStrategy implements TravellingStrategy {
+    private final ArrayList<Route> routes = new ArrayList<>(List.of(
+            new Route("İstanbul", 1),
+            new Route("Çanakkale", 2),
+            new Route("Muğla", 3),
+            new Route("Antalya", 4)
+    ));
+
     @Override
     public void travel() {
-        System.out.println("Traveling by boat to İstanbul, Çanakkale, Muğla, Antalya");
+        routes.stream()
+                .sorted(Comparator.comparingInt(Route::getPriority))
+                .forEach(route -> {
+            System.out.println("Traveling by boat to " + route.getCity());
+        });
     }
 }
--- TicketSystemPattern/src/main/java/com/tahabozdemir/ticketsystempattern/strategy/impl/PlaneStrategy.java
+++ TicketSystemPattern/src/main/java/com/tahabozdemir/ticketsystempattern/strategy/impl/PlaneStrategy.java
@@ -1,13 +1,27 @@
 package com.tahabozdemir.ticketsystempattern.strategy.impl;
 
+import com.tahabozdemir.ticketsystempattern.model.Route;
 import com.tahabozdemir.ticketsystempattern.strategy.TravellingStrategy;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 @Component
 public class PlaneStrategy implements TravellingStrategy {
+    private final ArrayList<Route> routes = new ArrayList<>(List.of(
+            new Route("İstanbul", 1),
+            new Route("Dalaman", 2),
+            new Route("Antalya", 3)
+    ));
+
     @Override
     public void travel() {
-        System.out.println("Traveling by plane to " +
-                "İstanbul, Dalaman, Antalya ");
+        routes.stream()
+                .sorted(Comparator.comparingInt(Route::getPriority))
+                .forEach(route -> {
+                    System.out.println("Traveling by plane to " + route.getCity());
+                });
     }
 }
